7a5d7378b4c7efad133738990b8e17eb99b87376
Ben-Peters <49176877+Ben-Peters@users.noreply.github.com>
2021-03-08 21:07:19 -0500

    add calender and start on main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc, char** argv) {
+    char* filePath = "./calendar.ics";
+    ifstream iCal;
+    if (argc == 1)
+        filePath = argv[0];
+    cout << filePath << endl;
+    iCal.open(filePath,ios::in);
+    if(iCal.is_open()) {
+        string line;
+        while (getline(iCal, line)) {
+            cout << line << endl;
+        }
+    }
+    else{
+        cout << "Unable to open file" << endl;
+        return -1;
+    }
+    iCal.close();
     return 0;
 }
