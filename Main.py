1244b8e1c75edbb3720ff4a7468538df0ca60108
Ben-Peters <49176877+Ben-Peters@users.noreply.github.com>
2021-03-10 22:57:08 -0500

    looks like it works mostly some weird edge cases.

--- Main.py
+++ Main.py
@@ -26,15 +26,17 @@ else:
 
 class When2Meet:
     """A class to hold all information about a when2meet"""
-    name = ''
-    url = ''
-    days = 0
-    startTime, endTime = datetime.now(), datetime.now()
-    hours = endTime - startTime
-    availability = []
-    eventID = 0
-    slots = []
-    personID = 0
+
+    def __init__(self):
+        self.name = ''
+        self.url = ''
+        self.days = 0
+        self.startTime, self.endTime = datetime.now(), datetime.now()
+        self.hours = 0
+        self.availability = []
+        self.eventID = 0
+        self.slots = []
+        self.personID = 0
 
     def calculateLength(self):
         self.startTime.replace(tzinfo=datetime.now().astimezone().tzinfo)
@@ -42,8 +44,8 @@ class When2Meet:
         self.days = (self.endTime - self.startTime).days + 1
         self.hours = (self.endTime.hour - self.startTime.hour)
         # self.slots = (self.startTime-datetime(1970, 1, 1)+timedelta(hours=5)).total_seconds()
-        print(self.slots)
-        print(self.hours)
+        # print(self.slots)
+        # print(self.hours)
 
     def initAvailability(self):
         for i in range(self.days * self.hours * 4):
@@ -60,33 +62,98 @@ class When2Meet:
             endTime = endTime.replace(hour=self.endTime.hour, minute=self.endTime.minute)
         if startTime.replace(day=1, month=1, year=1) < self.startTime.replace(day=1, month=1, year=1):
             startTime = startTime.replace(hour=self.startTime.hour, minute=self.startTime.minute)
-        hours = int((endTime-startTime).seconds / 3600)
+        hours = int((endTime - startTime).seconds / 3600)
         minutes = int(((endTime - startTime).seconds - (hours * 3600)) / 60)
         length = (hours * 4) + int((minutes + 14) / 15)
-        start = int(((startTime-self.startTime).days * self.hours * 4) +
+        start = int(((startTime - self.startTime).days * self.hours * 4) +
                     (startTime - self.startTime).seconds / 3600 * 4)
         for i in range(length):
-            self.availability[i+start] = "0"
-            #self.slot = (startTime-datetime(1970, 1, 1) + timedelta(hours=5)).total_seconds()
-            #postAvail(self)
+            self.availability[i + start] = "0"
+            # self.slot = (startTime-datetime(1970, 1, 1) + timedelta(hours=5)).total_seconds()
+            # postAvail(self)
         return
 
+    def parseHTML(self, filepath):
+        self.url = 'https://www.when2meet.com/?' + filepath
+        file = open(filepath, 'r')
+        lastLine = ''
+        twoAgo = ''
+        timeArray = False
+        while True:
 
-def parseCal(filepath):
-    iCalStream = open("calendar.ics").read()
+            line = file.readline()
 
-    cal = vobject.readOne(iCalStream)
-    for event in cal.contents.get('vevent'):
+            if not line:
+                break
+            if '<title>' in line:
+                self.name = line.split('<title>')[1].split(' - When2meet')[0]
+                print(self.name)
+            if "TimeOfSlot[0]" in line:
+                self.startTime = datetime.fromtimestamp(int(line.split('=')[1].split(";")[0]))
+                # self.slots.append(line.split('=')[1].split(";")[0] + '%')
+                print(self.startTime)
+                timeArray = True
+            if "TimeOfSlot[" in line and timeArray:
+                self.slots.append(line.split('=')[1].split(";")[0] + '%')
+            if ("var AvailableIDs=new Array();" in line and "TimeOfSlot" in twoAgo) or \
+                    ("PeopleNames[0]" in line and "TimeOfSlot" in twoAgo):
+                self.endTime = datetime.fromtimestamp(int(twoAgo.split('=')[1].split(";")[0])) + timedelta(
+                    minutes=15)
+                print(self.endTime)
+                break
+            twoAgo = lastLine
+            lastLine = line
+        self.calculateLength()
+        self.initAvailability()
+        self.initEventID()
+        file.close()
+        return
 
-        if verbosity == 2:
-            try:
-                print('Summary: ', event.summary.valueRepr())
-                print('Description: ', event.description.valueRepr())
-                print('Time (as a datetime object): ', event.dtstart.value)
-                print('-------------------------------------------------\n')
-            except AttributeError:
-                print("Something wasn't found")
-    return
+    def updateAvail(self, cal):
+        events = recurring_ical_events.of(cal).between(self.startTime, self.endTime)
+        for event in events:
+            if event.get('X-MICROSOFT-CDO-BUSYSTATUS') == 'BUSY':
+                if (self.startTime.replace(day=1, month=1, year=1) <=
+                    event.get('dtstart').dt.replace(tzinfo=None, day=1, month=1, year=1) < self.endTime.replace(day=1, month=1, year=1)) or \
+                        (event.get('dtstart').dt.replace(tzinfo=None, day=1, month=1, year=1) < self.endTime.replace(
+                            day=1, month=1, year=1)):
+                    self.setUnavailable(event.get('dtstart').dt, event.get('dtend').dt)
+                    print(event.get('summary'), end='')
+                    print(': ', end='')
+                    print(event.get('dtstart').dt)
+
+        return
+
+    def login(self):
+        file = open('creds.jpg', 'r')
+        name = file.readline().split('\n')[0]
+        password = file.readline().split('\n')[0]
+        meetingID = self.eventID
+        file.close()
+        url = 'https://www.when2meet.com/ProcessLogin.php'
+        formData = {'id': meetingID,
+                    'name': name,
+                    'password': password,
+                    '_': ''}
+        response = requests.post(url, data=formData)
+        self.personID = response.text
+        # print(response.text)
+        return
+
+    def postAvail(self):
+        url = 'https://www.when2meet.com/SaveTimes.php'
+
+        formData = {'person': self.personID,
+                    'event': self.eventID,
+                    'slots': ''.join(self.slots),
+                    'availability': ''.join(self.availability),
+                    '_': ''}
+        response = requests.post(url, data=formData)
+        # print(response)
+        return
+
+    def clear(self):
+        self.__init__()
 
 
 def getFromWeb(filepath):
@@ -120,88 +187,6 @@ def debugPrint(msg):
         print(msg)
 
 
-def addMeetingData(filepath, meeting):
-    meeting.url = 'https://www.when2meet.com/?' + filepath
-    file = open(filepath, 'r')
-    lastLine = ''
-    twoAgo = ''
-    timeArray = False
-    while True:
-
-        line = file.readline()
-
-        if not line:
-            break
-        if '<title>' in line:
-            meeting.name = line.split('<title>')[1].split(' - When2meet')[0]
-            print(meeting.name)
-        if "TimeOfSlot[0]" in line:
-            meeting.startTime = datetime.fromtimestamp(int(line.split('=')[1].split(";")[0]))
-            # meeting.slots.append(line.split('=')[1].split(";")[0] + '%')
-            print(meeting.startTime)
-            timeArray = True
-        if "TimeOfSlot[" in line and timeArray:
-            meeting.slots.append(line.split('=')[1].split(";")[0] + '%')
-        if ("var AvailableIDs=new Array();" in line and "TimeOfSlot" in twoAgo) or \
-                ("PeopleNames[0]" in line and "TimeOfSlot" in twoAgo):
-            meeting.endTime = datetime.fromtimestamp(int(twoAgo.split('=')[1].split(";")[0])) + timedelta(minutes=15)
-            print(meeting.endTime)
-            break
-        twoAgo = lastLine
-        lastLine = line
-    meeting.calculateLength()
-    meeting.initAvailability()
-    meeting.initEventID()
-    file.close()
-    return
-
-
-def updateMeeting(cal, meeting):
-    events = recurring_ical_events.of(cal).between(meeting.startTime, meeting.endTime)
-    for event in events:
-        if event.get('X-MICROSOFT-CDO-BUSYSTATUS') == 'BUSY':
-            if meeting.startTime.replace(day=1, month=1, year=1) <= \
-                    event.get('dtstart').dt.replace(tzinfo=None, day=1, month=1, year=1) < \
-                    meeting.endTime.replace(day=1, month=1, year=1) or \
-                    (event.get('dtend').dt.replace(tzinfo=None, day=1, month=1, year=1) > meeting.startTime.replace(day=1, month=1, year=1) > event.get('dtstart').dt.replace(tzinfo=None, day=1, month=1, year=1)):
-                meeting.setUnavailable(event.get('dtstart').dt, event.get('dtend').dt)
-                print(event.get('summary'), end='')
-                print(': ', end='')
-                print(event.get('dtstart').dt)
-
-    return
-
-
-def login(meeting):
-    file = open('creds.jpg', 'r')
-    name = file.readline().split('\n')[0]
-    password = file.readline().split('\n')[0]
-    meetingID = meeting.eventID
-    file.close()
-    url = 'https://www.when2meet.com/ProcessLogin.php'
-    formData = {'id': meetingID,
-                'name': name,
-                'password': password,
-                '_': ''}
-    response = requests.post(url, data=formData)
-    meeting.personID = response.text
-    print(response.text)
-    return
-
-
-def postAvail(meeting):
-    url = 'https://www.when2meet.com/SaveTimes.php'
-
-    formData = {'person': meeting.personID,
-                'event': meeting.eventID,
-                'slots': meeting.slots,
-                'availability': ''.join(meeting.availability),
-                '_': ''}
-    response = requests.post(url, data=formData)
-    # print(response)
-    return
-
-
 def cleanupFiles(fileList):
     # todo
     return
@@ -212,13 +197,15 @@ def main():
     iCalStream = open(static).read()
     cal = icalendar.Calendar.from_ical(iCalStream)
     # cal = calObj.contents.get('vevent')
-    meetings = [When2Meet for i in range(len(when2meetFiles))]
-    for file, meeting in zip(when2meetFiles, meetings):
-        meeting = When2Meet()
-        addMeetingData(file, meeting)
-        login(meeting)
-        updateMeeting(cal, meeting)
-        postAvail(meeting)
+    meetings = [When2Meet() for i in range(len(when2meetFiles))]
+    for i in range(len(when2meetFiles)):
+        meeting = meetings[i]
+        file = when2meetFiles[i]
+        meeting.parseHTML(file)
+        meeting.login()
+        meeting.updateAvail(cal)
+        meeting.postAvail()
+        meeting.clear()
     cleanupFiles(when2meetFiles)
     return
 
